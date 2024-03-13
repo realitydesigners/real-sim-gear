2e080d57c4c9ecbe82c42d9b919a2b20ece3e1db
RD <raymond.luke.spartz@gmail.com>
2024-03-13 00:19:36 -0700

    create next app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import Spline from "@splinetool/react-spline";
+export default function Test() {
+  return (
+    <main className="flex min-h-screen w-full h-screen max-h-screen">
+      <Spline scene="https://prod.spline.design/7zS2nYRakgBPQM-z/scene.splinecode" />
+    </main>
+  );
+}
